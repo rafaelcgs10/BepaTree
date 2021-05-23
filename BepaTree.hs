cf6158f840530f9e2c1d5cbcf07e6d8cb1388caf
rafaelcgs10 <rafaelcgs10@gmail.com>
2021-05-23 15:22:11 -0300

    Part one of the task

--- BepaTree.hs
+++ BepaTree.hs
@@ -1,7 +1,5 @@
 import qualified Data.Set as Set
 import Data.Set (fromList, Set)
-import Test.QuickCheck
-import GDP
 
 data NodeInfo = NodeInfo { cost :: Cost
                          , nodeInfoName :: NodeName
@@ -15,4 +13,22 @@ data Tree = Tree_TypeA NodeInfo String [Tree] | Tree_TypeB TypeB deriving (Show,
 data TypeB = TypeB Cost NodeName [TypeB] deriving (Show, Eq, Ord)
 
 getCommonNodeNamesExceptBepa :: Tree -> Tree -> [NodeName]
-getCommonNodeNamesExceptBepa _ _ = undefined
+getCommonNodeNamesExceptBepa tree1 tree2 = Set.toList (Set.intersection (getNodeNamesTreeExceptBepa tree1) (getNodeNamesTreeExceptBepa tree2))
+
+getNodeNamesTreeExceptBepa :: Tree -> Set NodeName
+getNodeNamesTreeExceptBepa (Tree_TypeA nodeInfo _ listTree) = case (nodeInfoName nodeInfo) of
+  (NodeName "Bepa") -> getNodeNamesListTreeExceptBepa listTree
+  nodeName -> Set.insert nodeName (getNodeNamesListTreeExceptBepa listTree)
+getNodeNamesTreeExceptBepa (Tree_TypeB typeB) = getNodeNamesTypeBExceptBepa typeB
+
+getNodeNamesListTreeExceptBepa :: [Tree] -> Set NodeName
+getNodeNamesListTreeExceptBepa (x:xs) = Set.union (getNodeNamesTreeExceptBepa x) $ getNodeNamesListTreeExceptBepa xs
+getNodeNamesListTreeExceptBepa [] = Set.empty
+
+getNodeNamesTypeBExceptBepa :: TypeB -> Set NodeName
+getNodeNamesTypeBExceptBepa (TypeB _ (NodeName "Bepa") listTypeB) = getNodeNamesListTypeBExceptBepa listTypeB
+getNodeNamesTypeBExceptBepa (TypeB _ nodeName listTypeB) = Set.insert nodeName (getNodeNamesListTypeBExceptBepa listTypeB)
+
+getNodeNamesListTypeBExceptBepa :: [TypeB] -> Set NodeName
+getNodeNamesListTypeBExceptBepa (x:xs) = Set.union (getNodeNamesTypeBExceptBepa x) $ getNodeNamesListTypeBExceptBepa xs
+getNodeNamesListTypeBExceptBepa [] = Set.empty
